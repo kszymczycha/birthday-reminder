c7a5cc60b4bfd678916b4ebd23a15a0781e709aa
kszymczycha <kszymczycha@smarthub.pl>
2023-05-03 12:39:59 +0200

    Update routing

--- src/layouts/Navbar.tsx
+++ src/layouts/Navbar.tsx
@@ -9,14 +9,14 @@ const Navbar = () => {
         <>
             <nav className={style['navbar']}>
                 <ul className={style['navbar-list']}>
-                    <li className={style['navbar-list-item']}><Link className={style['navbar-list-item-link']} to="/">Home</Link></li>
+                    <li className={style['navbar-list-item']}><Link className={style['navbar-list-item-link']} to="/birthday-reminder">Home</Link></li>
                     <li className={style['navbar-list-item']}><Link className={style['navbar-list-item-link']} to="/birthday-form">New reminder</Link></li>
                     <li className={style['navbar-list-item']}><Link className={style['navbar-list-item-link']} to="/calendar">Calendar</Link></li>
                 </ul>
             </nav>
             <div className={style['pages']}>
                 <Routes>
-                    <Route path="/" element={<Home />} />
+                    <Route path="/birthday-reminder" element={<Home />} />
                     <Route path="/birthday-form" element={<BirthdayForm />} />
                     <Route path="/calendar" element={<Calendar />} />
                 </Routes>
